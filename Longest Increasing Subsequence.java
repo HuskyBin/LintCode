55d3478f64124eb5b89f3259c10ceb5f8ec4c5bb
HuskyBin <xiang.zh@husky.neu.edu>
2014-10-31 15:04:15 -0400

    Create Longest Increasing Subsequence.java

---
+++ Longest Increasing Subsequence.java
@@ -0,0 +1,40 @@
+/*
+Given a sequence of integers, find the longest increasing subsequence (LIS).
+
+You code should return the length of the LIS.
+
+Example
+For [5, 4, 1, 2, 3], the LIS  is [1, 2, 3], return 3
+
+For [4, 2, 4, 5, 3, 7], the LIS is [4, 4, 5, 7], return 4
+
+
+*/
+public class Solution {
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int result = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] <= nums[i]) {
+                    result = Math.max(result, dp[j]);
+                }
+            }
+            dp[i] = result + 1;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
+
