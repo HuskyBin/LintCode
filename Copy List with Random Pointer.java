8f9598c57e3214aa16b8ed6493b8336a1244fb57
HuskyBin <xiang.zh@husky.neu.edu>
2014-12-16 15:48:54 -0500

    Update Copy List with Random Pointer.java

--- Copy List with Random Pointer.java
+++ Copy List with Random Pointer.java
@@ -44,3 +44,68 @@ public class Solution {
         return map.get(head.label);
     }
 }
+
+
+// Space Complexity O(1)
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    /**
+     * @param head: The head of linked list with a random pointer.
+     * @return: A new head of a deep copy of the list.
+     */
+    public RandomListNode copyRandomList(RandomListNode head) {
+        // write your code here
+        if (head == null) {
+            return head;
+        }
+       RandomListNode pNode = head;
+       insertCopyNode(pNode);
+       updateRandomPointer(pNode);
+       RandomListNode copyRoot = splitList(pNode);
+       return copyRoot;
+    }
+    
+    private void insertCopyNode(RandomListNode pNode) {
+        while (pNode != null) {
+           RandomListNode nextNode = pNode.next;
+           RandomListNode copyNode = new RandomListNode(pNode.label);
+           pNode.next = copyNode;
+           copyNode.next = nextNode;
+           pNode = nextNode;
+       }
+    }
+    
+    private void updateRandomPointer(RandomListNode pNode) {
+        while (pNode != null) {
+            RandomListNode nextNode = pNode.next.next;
+            RandomListNode copyNode = pNode.next;
+            if (pNode.random != null) {
+                copyNode.random = pNode.random.next;
+            }
+            pNode = nextNode;
+        }
+    }
+    
+    private RandomListNode splitList(RandomListNode pNode) {
+        RandomListNode nodeOne = pNode;
+        RandomListNode nodeTwo = pNode.next;
+        pNode = pNode.next.next;
+        nodeOne.next = null;
+        RandomListNode copyRoot = nodeTwo;
+        while (pNode != null) {
+            nodeOne.next = pNode;
+            nodeTwo.next = pNode.next;
+            pNode = pNode.next.next;
+            nodeOne = nodeOne.next;
+            nodeTwo = nodeTwo.next;
+        }
+        return copyRoot;
+    }
+}
