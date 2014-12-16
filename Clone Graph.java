eed4b5b51286fa8d75aafc61e39f30d8eb00e12d
HuskyBin <xiang.zh@husky.neu.edu>
2014-12-16 14:19:14 -0500

    Create Clone Graph.java

---
+++ Clone Graph.java
@@ -0,0 +1,59 @@
+/*
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+Example
+*/
+
+
+//DFS  will  over time limit
+public class Solution {
+    /**
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // write your code here
+        if (node == null) {
+            return node;
+        }
+        Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+        UndirectedGraphNode copyRoot = cloneGraphHelper(node, map);
+        return copyRoot;
+    }
+    
+    private UndirectedGraphNode cloneGraphHelper(UndirectedGraphNode pNode, Map<Integer, UndirectedGraphNode> map) {
+        UndirectedGraphNode copyNode = new UndirectedGraphNode(pNode.label);
+        map.put(pNode.label, copyNode);
+        ArrayList<UndirectedGraphNode> neighbors = pNode.neighbors;
+        for (UndirectedGraphNode node : neighbors) {
+            if (map.containsKey(node.label)) {
+                copyNode.neighbors.add(map.get(node.label));
+            }
+            else {
+                UndirectedGraphNode copyNeighborNode = cloneGraphHelper(node, map);
+                copyNode.neighbors.add(copyNeighborNode);
+            }
+        }
+        return copyNode;
+    }
+}
