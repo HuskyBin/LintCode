c5f548135612ea73a20e799f8da3aa3363051255
HuskyBin <xiang.zh@husky.neu.edu>
2014-12-18 15:09:27 -0500

    Update Jump Game II.java

--- Jump Game II.java
+++ Jump Game II.java
@@ -51,3 +51,39 @@ public class Solution {
         return result;
     }
 }
+
+
+// DP
+public class Solution {
+    /**
+     * @param A: A list of lists of integers
+     * @return: An integer
+     */
+    public int jump(int[] A) {
+        // write your code here
+        if (A == null) {
+            return 0;
+        }
+        if (A.length == 0 || A.length == 1) {
+            return 0;
+        }
+        int[] minJump = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            minJump[i] = -1;
+        }
+        minJump[0] = 0;
+        for (int i = 1; i < A.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (minJump[j] >= 0 && (A[j] + j >= i)) {
+                    if (minJump[i] == -1) {
+                        minJump[i] = minJump[j] + 1;
+                    }
+                    else {
+                        minJump[i] = Math.min(minJump[i], minJump[j] + 1);
+                    }
+                }
+            }
+        }
+        return minJump[A.length - 1];
+    }
+}
