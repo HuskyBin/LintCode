93e6086d39a4e34033f54bcb056ceb5b216fe784
HuskyBin <xiang.zh@husky.neu.edu>
2014-12-19 16:07:02 -0500

    Update k Sum.java

--- k Sum.java
+++ k Sum.java
@@ -49,3 +49,44 @@ public class Solution {
     }
    
 }
+
+
+// 3d dp
+public class Solution {
+    /**
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return an integer
+     */
+    public int result = 0;
+    public int kSum(int[] A,int k,int target) {
+        // write your code here
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        
+        int[][][] dp = new int[A.length + 1][k + 1][target + 1];
+        for (int i = 1; i <= A.length; i++) {
+            if (A[i - 1] <= target) {
+                for (int j = i; j <= A.length; j++) {
+                    dp[j][1][A[i - 1]] = 1;
+                }
+            }
+        }
+        
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 2; j <= Math.min(i, k); j++) {
+                for (int m = 1; m <= target; m++) {
+                    dp[i][j][m] = dp[i - 1][j][m];
+                    if (m - A[i - 1] >= 0) {
+                        dp[i][j][m] += dp[i - 1][j - 1][m - A[i - 1]];
+                    }
+                }
+            }
+        }
+        return dp[A.length][k][target];
+    }
+   
+}
+
