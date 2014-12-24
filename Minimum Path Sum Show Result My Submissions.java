b182ead43fd5e95ab9c67694fe829da3e84fe0b7
HuskyBin <xiang.zh@husky.neu.edu>
2014-12-24 17:38:19 -0500

    Create Minimum Path Sum Show Result My Submissions.java

---
+++ Minimum Path Sum Show Result My Submissions.java
@@ -0,0 +1,40 @@
+/*
+Easy Minimum Path Sum Show Result My Submissions
+
+38% Accepted
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note
+You can only move either down or right at any point in time.
+
+Example
+*/
+public class Solution {
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+}
+
