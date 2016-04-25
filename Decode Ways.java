fb79c0ecad54ba24ca2141efc284455807116a5a
HuskyBin <xiang.zh@husky.neu.edu>
2016-04-24 23:26:31 -0400

    Update Decode Ways.java

--- Decode Ways.java
+++ Decode Ways.java
@@ -59,3 +59,31 @@ public class Solution {
         }
     }
 }
+
+
+// Second Solution
+public class Solution {
+    /**
+     * @param s a string,  encoded message
+     * @return an integer, the number of ways decoding
+     */
+    public int numDecodings(String s) {
+        // Write your code hereo
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            } 
+            dp[i] = dp[i + 1];
+            if ((i < s.length() - 1) && (s.charAt(i) == '1' || ((s.charAt(i) == '2') && (s.charAt(i + 1) < '7')))) {
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+}
